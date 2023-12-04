caff5b1d72950b2bff05957bc640cff72b55e263
Arij Thabet <arijthabet01@gmail.com>
2023-12-04 21:45:24 +0100

    added role to the user

--- ProjetTp/Models/User.cs
+++ ProjetTp/Models/User.cs
@@ -11,7 +11,8 @@ namespace ProjetTp.Models
         public string? UserName { get; set; }
         public string? UserEmail { get; set; }
         public string? Password { get; set; }
-
+        //1 for admin, 2 for pharmacist, 3 for doctor
+        public int Role {  get; set; }
 
     }
 }
