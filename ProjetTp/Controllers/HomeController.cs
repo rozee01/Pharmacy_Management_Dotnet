547526a887b6bfc2ee945c56f0e140ed25e42bb4
Sabaa-Abdennabi <abdennabisabaa02@gmail.com>
2024-01-17 17:42:46 +0100

    added fields of the navbar

--- ProjetTp/Controllers/HomeController.cs
+++ ProjetTp/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjetTp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
