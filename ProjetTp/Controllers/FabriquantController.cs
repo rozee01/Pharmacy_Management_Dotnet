3f40c33d7a3171853e3a72025fbf15f320bffa32
farah <farahtrigui5@gmail.com>
2024-01-17 14:29:23 +0100

    controller methods

---
+++ ProjetTp/Controllers/FabriquantController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetTp.Models;
+
+namespace ProjetTp.Controllers
+{
+    public class FabriquantController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public FabriquantController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // GET: FabriquantController
+        public ActionResult Index()
+        {
+            var fabriquants = _context.Fabriquants.ToList();
+            return View(fabriquants);
+        }
+
+        // GET: FabriquantController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FabriquantController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FabriquantController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabriquant fabriquant)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Fabriquants.Add(fabriquant);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fabriquant);
+        }
+
+        // GET: FabriquantController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var fabriquant = _context.Fabriquants.Find(id);
+
+            if (fabriquant == null)
+            {
+                return NotFound();
+            }
+
+            return View(fabriquant);
+        }
+
+        // POST: FabriquantController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Fabriquant fabriquant)
+        {
+            if (id != fabriquant.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(fabriquant);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fabriquant);
+        }
+
+        // GET: FabriquantController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var fabriquant = _context.Fabriquants.Find(id);
+
+            if (fabriquant == null)
+            {
+                return NotFound();
+            }
+
+            return View(fabriquant);
+        }
+
+        // POST: FabriquantController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Fabriquant fab)
+        {
+            var fabriquant = _context.Fabriquants.Find(id);
+
+            if (fabriquant != null)
+            {
+                _context.Fabriquants.Remove(fabriquant);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProjetTp/Controllers/MedicamentController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetTp.Models;
+
+namespace ProjetTp.Controllers
+{
+    public class MedicamentController : Controller
+
+    {
+        private readonly AppDbContext _context; // Assuming you have a DbContext named ApplicationDbContext
+
+        public MedicamentController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // GET: MedicamentController
+        public ActionResult Index()
+        {
+            var medicaments = _context.Medicaments.ToList();
+            return View(medicaments);
+        }
+
+        // GET: MedicamentController/Details/5
+        public ActionResult Details(int id)
+        {
+            var medicament = _context.Medicaments.Find(id);
+
+            if (medicament == null)
+            {
+                return NotFound();
+            }
+
+            return View(medicament);
+        }
+
+        // GET: MedicamentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MedicamentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Medicament medicament)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Medicaments.Add(medicament);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medicament);
+        }
+
+        // GET: MedicamentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MedicamentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Medicament medicament)
+        {
+         
+            if (ModelState.IsValid)
+            {
+                _context.Update(medicament);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medicament);
+        }
+
+        // GET: MedicamentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var medicament = _context.Medicaments.Find(id);
+
+            if (medicament == null)
+            {
+                return NotFound();
+            }
+
+            return View(medicament);
+        }
+
+        // POST: MedicamentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Medicament collection)
+        {
+            var medicament = _context.Medicaments.Find(id);
+
+            if (medicament != null)
+            {
+                _context.Medicaments.Remove(medicament);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
